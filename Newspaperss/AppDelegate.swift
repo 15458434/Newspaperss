f00b4aa8e905b2da051c096571205427906b6141
Mark Cornelisse <m.p.cornelisse@gmail.com>
2023-07-30 18:48:39 +0200

    - Update to the correct app id.

--- Newspaperss/AppDelegate.swift
+++ Newspaperss/AppDelegate.swift
@@ -13,6 +13,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UIApplicationDelegate
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
         GADMobileAds.sharedInstance().start()
         return true
     }
